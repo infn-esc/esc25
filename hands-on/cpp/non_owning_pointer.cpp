fd771b59026f8a79476b09949c04bf8e50d3f1de
Francesco Giacomini <francesco.giacomini@cnaf.infn.it>
2025-09-30 08:33:12 +0200

    Add exercises for memory issues

---
+++ hands-on/cpp/non_owning_pointer.cpp
@@ -0,0 +1,22 @@
+#include <memory>
+#include <cstdlib>
+#include <cstring>
+
+char* some_api();
+
+int main()
+{
+  auto* p = some_api();
+
+  // std::free(p);
+  // delete p;
+  // delete [] p;
+}
+
+
+#include <cstring>
+
+char* some_api() {
+  static char s[] = "Hello, world!";
+  return std::strstr(s, "orl");
+}
